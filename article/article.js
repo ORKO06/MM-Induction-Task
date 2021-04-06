29dc55a9e28ff1b6347a7edfc6ed184eb829f108
Hemant Bajaj <41hemu38@gmail.com>
2021-04-06 21:40:13 +0530

    add like functionality

--- article/article.js
+++ article/article.js
@@ -13,9 +13,21 @@ document.querySelector("#btn").addEventListener("click", function () {
   document.querySelector(`.user-comment${um}`).style.width = "80%";
 
   document.querySelector(`.user-comment${um}`).style.margin = "auto";
+  document.querySelector(`.Comment-display`).style.height = `${150 * um}px`;
 
   document.querySelector(".comment-left").style.height = document.querySelector(
     ".comment-right"
   ).style.height;
   um++;
+});
+
+document.querySelector(".like").addEventListener("click", function () {
+  if (Number(document.querySelector(".like").textContent == 106)) {
+    document.querySelector(".like").textContent = "105";
+
+    document.querySelector(".like").style.color = "red";
+  } else {
+    document.querySelector(".like").textContent = "106";
+    document.querySelector(".like").style.color = "blue";
+  }
 });
