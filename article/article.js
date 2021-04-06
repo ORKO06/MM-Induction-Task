a2baa6c5f50e03fe82aa3a61e033835c14024331
Hemant Bajaj <41hemu38@gmail.com>
2021-04-06 18:16:53 +0530

    article view update

--- article/article.js
+++ article/article.js
@@ -1,13 +1,22 @@
+let um = 1;
+
 document.querySelector("#btn").addEventListener("click", function () {
-  document.querySelector(".comment-1").classList.remove("hidden");
-  document.querySelector(".user-comment").textContent = document.querySelector(
-    "#comment"
-  ).value;
+  document.querySelector(`.comment-${um}`).classList.remove("hidden");
+
+  document.querySelector(`.comment-${um}`).style.margin = "10px 0";
+  document.querySelector(
+    `.user-comment${um}`
+  ).textContent = document.querySelector("#comment").value;
   document.querySelector(".user-name").textContent = document.querySelector(
     "#name"
   ).value;
 
+  document.querySelector(`.user-comment${um}`).style.width = "80%";
+
+  document.querySelector(`.user-comment${um}`).style.margin = "auto";
+
   document.querySelector(".comment-left").style.height = document.querySelector(
     ".comment-right"
   ).style.height;
+  um++;
 });
